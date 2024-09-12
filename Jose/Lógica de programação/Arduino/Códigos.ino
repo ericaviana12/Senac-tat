d5f1b8f86832a224331c98fcb84f2ae6b77b0f00
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-09-12 20:22:12 -0300

    Create Códigos.ino

---
+++ Jose/Lógica de programação/Arduino/Códigos.ino
@@ -0,0 +1,7 @@
+Códigos Arduino e descrição
+
+---
+
+Serial.begin(9600) //Para o Arduino conseguir conversar com o console
+
+Serial.print("Hello World"); //Para o Arduino exibir o texto escrito no código
