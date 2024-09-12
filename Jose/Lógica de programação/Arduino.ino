0f6b9389fe7a06d4c4773ff32fa22af5fe6bf856
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-09-12 20:20:25 -0300

    Create Arduino.ino

---
+++ Jose/Lógica de programação/Arduino.ino
@@ -0,0 +1,26 @@
+Cabeçalho de documentação
+/**
+
+    Nome do projeto
+    Descrição da classe / função
+    @author Erica Viana
+    @version 1.0
+*/
+
+---
+
+O que é colocado nessa area do código é executado uma única vez
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+---
+
+O que é colocado nessa area do código é executado repetidas vezes, de acordo com o escolhido
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
