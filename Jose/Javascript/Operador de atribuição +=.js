af9606b7d13ba341ae6e426bb8807b624f84ec3a
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-09-24 20:50:05 -0300

    Create Operador de atribuição +=.js

---
+++ Jose/Javascript/Operador de atribuição +=.js
@@ -0,0 +1,18 @@
+// console.log(" Operador  |     Atribuição        ")
+// console.log("   +=      |  Somar e atribuir     ")
+
+---
+
+let y = 10 (aperta enter)
+
+y + 5
+15
+
+console.log(y)
+10
+
+y += 5
+15
+
+console.log(y)
+15
