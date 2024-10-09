8f47745a11fa6dc3b96d0869e2ba7147bf4d17de
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-10-08 22:12:13 -0300

    Update 00 - Base Arduino.ino

--- Jose/Lógica de programação/Arduino/00 - Base Arduino.ino
+++ Jose/Lógica de programação/Arduino/00 - Base Arduino.ino
@@ -32,3 +32,9 @@ Códigos Arduino e descrição
 Serial.begin (9600); // Para o Arduino conseguir conversar com o console
 
 Serial.print ("Hello World"); // Para o Arduino exibir o texto escrito no código
+
+---
+
+&& // AND
+|| // OR
+! // NOT
