d85b0c556175bf209afc116c567aba11b6a77bce
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-10-10 19:58:01 -0300

    Create If-else.js

---
+++ Jose/Lógica de programação/JavaScript/Estruturas de controle/If-else.js
@@ -0,0 +1,35 @@
+let media = 7
+undefined
+if (media <7) {
+    console.log("reprovado")
+}
+undefined
+if (media < 5) {
+    console.log("reprovado")
+}
+undefined
+let media = 3
+3
+if (media < 5) {
+    console.log("reprovado")
+}
+VM382:2 reprovado
+undefined
+
+---
+
+if (media < 5) {
+    console.log("reprovado")
+} else {
+    console.log("aprovado")
+}
+VM578:2 reprovado
+undefined
+let media = 9
+9
+if (media < 5) {
+    console.log("reprovado")
+} else {
+    console.log("aprovado")
+}
+VM600:4 aprovado
