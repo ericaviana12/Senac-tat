af9d8909fdec9445628698e788bffe136bf158aa
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-10-10 19:26:06 -0300

    Create if.js

---
+++ Jose/Javascript/if.js
@@ -0,0 +1,16 @@
+Estruturas de controle condicionais
+
+let media = 7 (aperta enter)
+
+if (media < 5) {
+    console.log("reprovado")
+}
+
+---
+
+let media = 3 (aperta enter)
+
+if (media < 5) {
+    console.log("reprovado")
+}
+  reprovado // É exibida a frase condicional por ser uma condição verdadeira
