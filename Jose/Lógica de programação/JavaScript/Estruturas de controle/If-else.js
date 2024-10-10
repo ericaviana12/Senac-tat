e91d3acbf74f7a89c15b7b4cb2147e73f91c5746
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-10-10 20:28:04 -0300

    Update If-else.js

--- Jose/Lógica de programação/JavaScript/Estruturas de controle/If-else.js
+++ Jose/Lógica de programação/JavaScript/Estruturas de controle/If-else.js
@@ -1,35 +1,50 @@
-let media = 7
-undefined
-if (media <7) {
-    console.log("reprovado")
-}
-undefined
+Estruturas de controle condicionais:
+
+- Somente com a sintaxe "if"
+
+
+// Exemplo de condição verdadeira (Média = 7)
+
+let media = 7 (aperta enter) // Dado atribuído para validação
+
 if (media < 5) {
     console.log("reprovado")
 }
-undefined
-let media = 3
-3
+
+
+// Exemplo de condição falsa (Média = 3)
+
+let media = 3 (aperta enter) // Dado atribuído para validação
+
 if (media < 5) {
     console.log("reprovado")
 }
-VM382:2 reprovado
-undefined
+    reprovado // Exibe a frase solicitada, devido ao valor atribuído ser menor que o condicional
 
 ---
 
+- Unir as sintaxes "if" e "else"
+
+
+// Exemplo de condição falsa (Média = 3)
+
+let media = 3 (aperta enter) // Dado atribuído para validação
+
 if (media < 5) {
     console.log("reprovado")
 } else {
     console.log("aprovado")
 }
-VM578:2 reprovado
-undefined
-let media = 9
-9
+    reprovado // Exibe a frase solicitada, devido ao valor atribuído ser menor que o condicional
+
+
+// Exemplo de condição verdadeira (Média = 9)
+
+let media = 9 (aperta enter) // Dado atribuído para validação
+
 if (media < 5) {
     console.log("reprovado")
 } else {
     console.log("aprovado")
 }
-VM600:4 aprovado
+    aprovado // Exibe a frase solicitada, devido à sintaxe "else"
