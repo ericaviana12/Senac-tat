e52d4950d452867f683689b79642908e1bab3792
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-10-15 22:20:46 -0300

    Update 00 - Cálculo do IMC.js

--- Jose/Lógica de programação/JavaScript/00 - Cálculo do IMC.js
+++ Jose/Lógica de programação/JavaScript/00 - Cálculo do IMC.js
@@ -17,23 +17,19 @@ let altura = Number(input.question("Digite a sua altura (em metros): "))
 // Processamento
 let imc = peso / Math.pow(altura, 2) // Fórmula do IMC: IMC = peso/altura²
 
-// Classificação do IMC
-let classificacao = ''
-
-if (imc < 18.5) {
-  classificacao = 'Abaixo do peso'
-} else if (imc >= 18.5 && imc < 24.9) {
-  classificacao = 'Peso normal'
-} else if (imc >= 25 && imc < 29.9) {
-  classificacao = 'Sobrepeso'
-} else if (imc >= 30 && imc < 34.9) {
-  classificacao = 'Obesidade grau 1'
-} else if (imc >= 35 && imc < 39.9) {
-  classificacao = 'Obesidade grau 2'
-} else {
-  classificacao = 'Obesidade grau 3 (mórbida)'
-}
-
 // Saída
 console.log(`Seu IMC é: ${imc.toFixed(2)}`)
-console.log(`Classificação: ${classificacao}`)
+
+if (imc < 18.5) {
+  console.log("Abaixo do peso")
+} else if (imc < 25) {
+  console.log("Peso normal")
+} else if (imc < 30) {
+  console.log("Sobrepeso")
+} else if (imc < 35) {
+  console.log("Obesidade grau 1")
+} else if (imc < 40) {
+  console.log("Obesidade grau 2")
+} else {
+  console.log("Obesidade grau 3 (mórbida)")
+}
