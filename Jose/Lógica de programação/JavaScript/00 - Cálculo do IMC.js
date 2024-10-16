60f8b8dcd9e0854a3c2c8c046a9283486638bc05
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-10-15 22:03:45 -0300

    Update 00 - Cálculo do IMC.js

--- Jose/Lógica de programação/JavaScript/00 - Cálculo do IMC.js
+++ Jose/Lógica de programação/JavaScript/00 - Cálculo do IMC.js
@@ -1,5 +1,5 @@
 /**
- * Cálculo do IMC
+ * Cálculo de IMC
  * @author Erica Viana
  */
 
@@ -15,7 +15,25 @@ let peso = Number(input.question("Digite o seu peso (em kg): "));
 let altura = Number(input.question("Digite a sua altura (em metros): "));
 
 // Processamento
-let imc = peso / Math.pow(altura, 2); // Fórmula do IMC: IMC = peso/altura², elevando a altura ao quadrado com a função Math.pow()
+let imc = peso / Math.pow(altura, 2); // Fórmula do IMC: IMC = peso/altura²
+
+// Classificação do IMC
+let classificacao = '';
+
+if (imc < 18.5) {
+  classificacao = 'Abaixo do peso';
+} else if (imc >= 18.5 && imc < 24.9) {
+  classificacao = 'Peso normal';
+} else if (imc >= 25 && imc < 29.9) {
+  classificacao = 'Sobrepeso';
+} else if (imc >= 30 && imc < 34.9) {
+  classificacao = 'Obesidade grau 1';
+} else if (imc >= 35 && imc < 39.9) {
+  classificacao = 'Obesidade grau 2';
+} else {
+  classificacao = 'Obesidade grau 3 (mórbida)';
+}
 
 // Saída
-console.log(`Seu IMC é: ${imc.toFixed(2)}`); // Exibe o resultado do IMC formatado com duas casas decimais
+console.log(`Seu IMC é: ${imc.toFixed(2)}`); 
+console.log(`Classificação: ${classificacao}`); 
